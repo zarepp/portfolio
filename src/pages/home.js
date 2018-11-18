f82046bb6cda1147eaf20fbcc36d87348ebbf1ef
Zharif Othman <zharifothman92@gmail.com>
2018-11-18 10:42:54 +0800

    update hard code data

--- src/pages/home.js
+++ src/pages/home.js
@@ -24,6 +24,7 @@ import docker from './../images/docker.png';
 import gmail from './../images/gmail.png';
 import linkedin from './../images/linkedin.png';
 import github from './../images/github.png';
+import coming from './../images/coming-soon.png';
 
 const resumeURL = "https://drive.google.com/uc?authuser=0&id=1JUYISK7xhqpSzZI3i9nWJuBGDYwaQKIO&export=download";
 
@@ -98,7 +99,20 @@ const styles = theme => ({
   },
 });
 
-const cards = [1, 2, 3, 4];
+const cards = [
+  {
+    heading: "API Endpoint",
+    description: "Express.js host to Firebase. Used for Mobile Apps and Arduino",
+    url: "google.com",
+    src: coming,
+  },
+  {
+    heading: "Chat Website",
+    description: "System is one of the sytem developed",
+    url: "google.com",
+    src: coming,
+  }
+];
 
 function handleDelete() {
   alert('You clicked the delete icon.'); // eslint-disable-line no-alert
@@ -167,28 +181,28 @@ function Album(props) {
         </div>
         <div className={classNames(classes.layout, classes.cardGrid)}>
           {/* End hero unit */}
-          <Grid container spacing={40}>
+          <Grid container justify="center" spacing={40}>
             {cards.map(card => (
-              <Grid item key={card} sm={6} md={4} lg={3}>
+              <Grid item key={card} sm={6} md={2} lg={3}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
+                    image={card.src}
+                    title="Coming Soon"
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      {card.heading}
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the content.
+                      {card.description}
                     </Typography>
                   </CardContent>
-                  <CardActions>
+                  {/* <CardActions>
                     <Button size="small" color="primary">
                       View
                     </Button>
-                  </CardActions>
+                  </CardActions> */}
                 </Card>
               </Grid>
             ))}
