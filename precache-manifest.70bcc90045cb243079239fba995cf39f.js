7b0039e2f36e08c551ba2fbd6d957eb8b8e2511d
Zharif Othman <zharifothman92@gmail.com>
2018-11-26 10:23:55 +0800

    Updates

---
+++ precache-manifest.70bcc90045cb243079239fba995cf39f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c939db093c6307c6adae2fb7be36740",
+    "url": "/portfolio/static/media/linux.0c939db0.png"
+  },
+  {
+    "revision": "24eebf170e9ebb45391c72a6dc541830",
+    "url": "/portfolio/static/media/linkedin.24eebf17.png"
+  },
+  {
+    "revision": "b3e6202f5356edebd8c8205623eef0f1",
+    "url": "/portfolio/static/media/docker.b3e6202f.png"
+  },
+  {
+    "revision": "80411a708fb7a5ba36a726d912cc3413",
+    "url": "/portfolio/static/media/coming-soon.80411a70.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c2d553c7d273ed15cbeb",
+    "url": "/portfolio/static/js/main.c2d553c7.chunk.js"
+  },
+  {
+    "revision": "43e623a7b3b1a8aa606c",
+    "url": "/portfolio/static/js/1.43e623a7.chunk.js"
+  },
+  {
+    "revision": "c2d553c7d273ed15cbeb",
+    "url": "/portfolio/static/css/main.db9af403.chunk.css"
+  },
+  {
+    "revision": "b9850dc10c8803568ed5aa55f7bb24d2",
+    "url": "/portfolio/index.html"
+  }
+];
